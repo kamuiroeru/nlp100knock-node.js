58dc6e7a3c17b1395ca81f91f128953d0c41a3ab
KamuiRoeru <me1802@s.akashi.ac.jp>
2019-02-17 21:12:56 +0900

    和差積集合を作成

--- section1/q06.js
+++ section1/q06.js
@@ -6,6 +6,24 @@ Set.prototype.union = function(setB) {
 	return union
 }
 
+Set.prototype.intersection = function(setB) {
+	let intersection = new Set()
+	for (let d of setB) {
+		if (this.has(d)){
+			intersection.add(d)
+		}
+	}
+	return intersection
+}
+
+Set.prototype.difference = function(setB) {
+	let difference = new Set(this)
+	for (let d of setB) {
+		difference.delete(d)
+	}
+	return difference
+}
+
 const ngram = require('./q05.js')
 
 let str1 = "paraparaparadise"
@@ -17,5 +35,6 @@ let Y = new Set(ngram(str2))
 console.log(X)
 console.log(Y)
 console.log(X.union(Y))
-
+console.log(X.intersection(Y))
+console.log(X.difference(Y))
 
